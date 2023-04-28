99b934bb2cf867df8710b7801edea59b469a5c31
Mohammad <mohammadnajafzade@yahoo.com>
2023-04-28 18:38:02 +0100

    Migrated from ngModule to standalone components and removed boiler plate codes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from "@angular/core";
+import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
+import { provideRouter } from "@angular/router";
+import { AppRouting } from "./app/app-routing";
+import { AppComponent } from "./app/app.component";
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(AppRouting), importProvidersFrom(BrowserModule)],
+}).catch((err) => console.error(err));
