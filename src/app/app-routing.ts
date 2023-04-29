6c842cc553da90eac59b693dc5d4160e413d24ee
Mohammad <mohammadnajafzade@yahoo.com>
2023-04-29 20:59:30 +0100

    Added league details component

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,3 +1,10 @@
 import { Routes } from "@angular/router";
+import { EuropeanLeaguesComponent } from "./european-leagues/european-leagues.component";
+import { LeagueDetailsComponent } from "./european-leagues/league-details/league-details.component";
 
-export const AppRouting: Routes = [{ path: "**", redirectTo: ".", pathMatch: "full" }];
+export const AppRouting: Routes = [
+  { path: "", redirectTo: "european-league", pathMatch: "full" },
+  { path: "european-league", component: EuropeanLeaguesComponent },
+  { path: "european-league/:leagueCode", component: LeagueDetailsComponent },
+  { path: "**", redirectTo: "european-league", pathMatch: "full" },
+];
---
+++ src/app/european-leagues/league-details/league-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: "app-league-details",
+  templateUrl: "./league-details.component.html",
+  styleUrls: ["./league-details.component.scss"],
+})
+export class LeagueDetailsComponent {}
