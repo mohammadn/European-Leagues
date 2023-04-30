d17dc9f2cbf8c387b800262da93b1a296ffd3462
Mohammad <mohammadnajafzade@yahoo.com>
2023-04-30 11:40:53 +0100

    Changed the way of using fontawesome in the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,12 @@
-import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { faFutbol } from "@fortawesome/free-solid-svg-icons";
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule, FontAwesomeModule],
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
+  imports: [CommonModule, RouterModule],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  protected faFutbol = faFutbol;
-}
+export class AppComponent {}
--- src/app/european-leagues/european-leagues.component.ts
+++ src/app/european-leagues/european-leagues.component.ts
@@ -1,24 +1,20 @@
-import { CommonModule } from "@angular/common";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { RouterModule } from "@angular/router";
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
-import { Subscription } from "rxjs";
-import { Competition } from "../shared-modules/models/competition.model";
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Competition } from '../shared-modules/models/competition.model';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, RouterModule, FontAwesomeModule],
-  selector: "app-european-leagues",
-  templateUrl: "./european-leagues.component.html",
-  styleUrls: ["./european-leagues.component.scss"],
+  imports: [CommonModule, RouterModule],
+  selector: 'app-european-leagues',
+  templateUrl: './european-leagues.component.html',
+  styleUrls: ['./european-leagues.component.scss'],
 })
 export class EuropeanLeaguesComponent implements OnInit, OnDestroy {
-  protected faChevronRight = faChevronRight;
-
   protected subscriptions = new Subscription();
-  protected topFiveEuropeanLeagueCodes = ["PL", "BL1", "PD", "SA", "FL1"];
+  protected topFiveEuropeanLeagueCodes = ['PL', 'BL1', 'PD', 'SA', 'FL1'];
   protected competitions: [Competition] | undefined;
 
   constructor(private http: HttpClient) {}
@@ -32,11 +28,11 @@ export class EuropeanLeaguesComponent implements OnInit, OnDestroy {
   }
 
   private loadCompetionsDetails(): void {
-    const headers = { headers: new HttpHeaders({ "X-Auth-Token": "75165ac7b00a48468b39ba671a4e675c" }) };
+    const headers = { headers: new HttpHeaders({ 'X-Auth-Token': '75165ac7b00a48468b39ba671a4e675c' }) };
 
     this.subscriptions.add(
       this.http
-        .get<{ counter: number; filters: any; competitions: [Competition] }>("api/competitions", headers)
+        .get<{ counter: number; filters: unknown; competitions: [Competition] }>('api/competitions', headers)
         .subscribe({
           next: (competitions) => {
             this.competitions = competitions.competitions;
